4a69be0b7d002ea99f1891084a2cdffae4b15d72
Maria Acosta <mava_19@hotmail.com>
2022-10-30 21:40:54 -0300

    SitioLubricentro

---
+++ SitioLubricentro/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require ('nodemailer')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var telefono = req.body.telefono;
+  var email = req.body.email;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: 'maria.a@gmail.com',
+    subjet: 'Contacto de la Web',
+    html: nombre + " " + apellido + " se contacto a traves de la web y quiere mas info a este correo: " + email + ". <br> Ademas, hizo el siguiente comentario:" + mensaje + ". <br> Su telefono es "+ telefono
+  }
+  
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    }
+  })
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message: 'ENVIO EXISTOSO',
+  });
+  
+})
+
+module.exports = router;
