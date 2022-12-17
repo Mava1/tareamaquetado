af18651a8516b7cbc7276da061b4e96dbbf3d0bd
Maria Acosta <mava_19@hotmail.com>
2022-12-17 17:16:35 -0300

    MOD5-U2

--- SitioLubricentro/routes/admin/novedades.js
+++ SitioLubricentro/routes/admin/novedades.js
@@ -1,27 +1,85 @@
 var express = require('express');
 var router = express.Router()
 var novedadesModel = require('../../models/novedadesModel');
-var util = require ('util');
+var util = require('util');
 
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 
-    var novedades = await novedadesModel.getNovedades();
-  
-    res.render('admin/novedades',{
-        layout: 'admin/layout',
-        persona: req.session.nombre,
-        novedades
-        
-    });
+  var novedades = await novedadesModel.getNovedades();
+
+  res.render('admin/novedades', {
+    layout: 'admin/layout',
+    persona: req.session.nombre,
+    novedades
+
   });
-
-
-router.get ('/eliminar/:id', async (req, res, next) =>{
-    const id = req.params.id;
-
-    await novedadesModel.deleteNovedadesById(id);
-    res.redirect ('/admin/novedades')
 });
 
-  module.exports = router;
+router.get('/eliminar/:id', async (req, res, next) => {
+  const id = req.params.id;
+
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+});
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
+
+router.post('/agregar', async (req, res, next) => {
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se cargo la informacion'
+    });
+  }
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.Titulo,
+      subtitulo: req.body.Subtitulo,
+      Cuerpo: req.body.Cuerpo
+
+    }
+    console.log(obj)
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  } catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: "No se modifico la novedad"
+
+    })
+  }
+});
+
+
+module.exports = router;
